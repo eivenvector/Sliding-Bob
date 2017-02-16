47dc710d03f97176022693ae9133e5aa9c10734e
Ivan Aguilar <aguilar@berkeley.edu>
2017-02-15 17:40:59 -0800

    Player actions are clearer. Textures are now instance variables. CurrentStatus instance variable added.

--- Sliding Bob/PlayerSpriteNode.swift
+++ Sliding Bob/PlayerSpriteNode.swift
@@ -11,51 +11,152 @@ import SpriteKit
 
 class PlayerSpriteNode: SKSpriteNode {
     
-    var animation: SKAction?
+    // MARK: Properties
     
-    // MARK: Movement
+    private var animation: SKAction?
+    var currentStatus: CurrentStatus = .Idle
+    var velocity = 0.0
     
-    func beginWalkingAnimation(time: TimeInterval) {
-        let textureAtlas = SKTextureAtlas(named: "player")
-        let frames = ["p1_walk01", "p1_walk02", "p1_walk03","p1_walk04",
-                      "p1_walk05", "p1_walk06", "p1_walk07", "p1_walk08",
-                      "p1_walk09", "p1_walk10", "p1_walk11"].map({textureAtlas.textureNamed($0)})
-        let animate = SKAction.animate(with: frames, timePerFrame: time)
-        let animation = SKAction.repeatForever(animate)
-        
-        run(animation, withKey: "walking")
-    }
+    // MARK: Textures
     
-    func beginJump() {
-        if (self.action(forKey: "walking") != nil) {
-            self.removeAction(forKey: "walking")
-            let impulse = CGVector(dx: 0.0, dy: 40.0)
-            self.texture = SKTexture(imageNamed: "p1_jump")
-            self.physicsBody?.applyImpulse(impulse)
+    private let standTexture = SKTexture(imageNamed: "p1_stand")
+    private let jumpTexture = SKTexture(imageNamed: "p1_jump")
+    private let duckTexture = SKTexture(imageNamed: "p1_duck")
+    private let textureAtlas = SKTextureAtlas(named: "player")
+    private let frames = ["p1_walk01", "p1_walk02", "p1_walk03",
+                          "p1_walk04", "p1_walk05", "p1_walk06",
+                          "p1_walk07", "p1_walk08", "p1_walk09",
+                          "p1_walk10", "p1_walk11"].map {
+                            SKTextureAtlas(named: "player").textureNamed($0)
+                            }
+    
+    
+    // MARK: Actions
+    
+    func beginStanding() {
+        if (self.currentStatus == .Idle) {
+            self.currentStatus = .Standing
+            self.texture = self.standTexture
+            self.velocity = PLAYER_SPEED
         } else {
             return
         }
     }
     
-    func beginDuckWithSize(size: CGSize, movingForward fwd:Bool = false) {
-        
-        let neg = (fwd ? Double(0) : Double(-1))
-        let duckingAction = SKAction.move(by:
-            CGVector(dx: PLAYER_SPEED * 60 * neg, dy: 0), duration: DUCK_TIME)
-        self.removeAction(forKey: "walking")
-        self.texture = SKTexture(imageNamed: "p1_duck")
-        self.size = size
-        self.setDuckingBody()
-        run(duckingAction, withKey: "ducking")
+    func endStanding() {
+        if (self.currentStatus == .Standing) {
+            self.currentStatus = .Idle
+            self.velocity = PLAYER_SPEED_IDLE
+        } else {
+            return
+        }
     }
     
-    func endDuckWithSize(size: CGSize) {
-        
-        self.size = size
-        self.setPhysicsBody()
-        self.removeAction(forKey: "ducking")
+    func beginWalking(time: TimeInterval) {
+        if (self.currentStatus == .Idle) {
+            let animate = SKAction.animate(with: self.frames, timePerFrame: time)
+            let animation = SKAction.repeatForever(animate)
+            
+            self.run(animation, withKey: "walking")
+        } else if (self.currentStatus == .Sliding) {
+            let animate = SKAction.animate(with: self.frames, timePerFrame: time)
+            let animation = SKAction.repeatForever(animate)
+            self.run(animation, withKey: "walking")
+        } else {
+            return
+        }
     }
     
+    func endWalking() {
+        if (self.currentStatus == .Walking) {
+            self.currentStatus = .Idle
+            self.removeAction(forKey: "walking")
+        } else {
+            return
+        }
+    }
+    
+    func beginJumping() {
+        if (self.currentStatus == .Idle) {
+            self.currentStatus = .Jumping
+            self.texture = self.jumpTexture
+            
+            let impulse = CGVector(dx: 0.0, dy: 40.0)
+            self.physicsBody?.applyImpulse(impulse)
+        } else {
+            return
+        }
+    }
+
+    func endJumping() {
+        if (self.currentStatus == .Jumping) {
+            self.currentStatus = .Idle
+        } else {
+            return
+        }
+    }
+    
+    func beginDucking() {
+        if (self.currentStatus == .Idle) {
+            self.currentStatus = .Ducking
+            self.texture = self.duckTexture
+            self.size = DUCK_SIZE
+            self.setDuckingBody()
+        } else if (self.currentStatus == .Sliding) {
+            self.texture = self.duckTexture
+            self.size = DUCK_SIZE
+            self.setDuckingBody()
+        } else {
+            return
+        }
+    }
+    
+    func endDucking() {
+        if (self.currentStatus == .Ducking) {
+            self.currentStatus = .Idle
+            self.size = STAND_SIZE
+            self.setPhysicsBody()
+        } else if (self.currentStatus == .Sliding) {
+            self.size = STAND_SIZE
+            self.setPhysicsBody()
+        } else {
+            return
+        }
+    }
+    
+    func beginSliding() {
+        if (self.currentStatus == .Idle) {
+            self.currentStatus = .Sliding
+            let slidingAction = SKAction.sequence([
+                SKAction.applyImpulse(SLIDE_RUN, duration: 1.0),
+                SKAction.run {
+                    self.beginWalking(time: 0.05)
+                },
+                SKAction.wait(forDuration: 0.30),
+                SKAction.run {
+                    self.removeAction(forKey: "walking")
+                },
+                SKAction.run {
+                    self.beginDucking()
+                },
+                SKAction.applyImpulse(SLIDE_RUN, duration: 1.0),
+                SKAction.run {
+                    self.endDucking()
+                }
+                ])
+            self.run(slidingAction, withKey: "sliding")
+        } else {
+            return
+        }
+    }
+    
+    func endSliding() {
+        if (self.currentStatus == .Sliding) {
+            self.currentStatus = .Idle
+            self.removeAction(forKey: "sliding")
+        }
+    }
+        
     // MARK: PhysicsBody
     
     func setPhysicsBody() {
@@ -89,4 +190,16 @@ class PlayerSpriteNode: SKSpriteNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented.")
     }
+    
+    // MARK: Status Enum
+    
+    enum CurrentStatus {
+        case Standing
+        case Walking
+        case Jumping
+        case Ducking
+        case Sliding
+        case Idle
+    }
+    
 }
